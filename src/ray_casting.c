07f06c0f42d3f6c180861a79b168741cc4a49086
Ahmed Sabri <asabri@e1r8p4.1337.ma>
2023-11-19 10:49:51 +0100

    [NOTE] : everything is work perfectly , we must norm the project to push

--- src/ray_casting.c
+++ src/ray_casting.c
@@ -6,27 +6,27 @@
 /*   By: asabri <asabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 21:47:33 by asabri            #+#    #+#             */
-/*   Updated: 2023/11/18 16:22:19 by asabri           ###   ########.fr       */
+/*   Updated: 2023/11/19 10:47:14 by asabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-double	ft_fabs(double dx, double dy)
+double  ft_fabs(double dx, double dy)
 {
-	if (fabs(dx) > fabs(dy))
-		return (fabs(dx));
-	else
-		return (fabs(dy));
+    if (fabs(dx) > fabs(dy))
+        return (fabs(dx));
+    else
+        return (fabs(dy));
 }
 
 void dda(t_data *data, double x1, double y1, double x2, double y2, int color)
 {
     int i;
     double steps;
-	double dx;
-	double dy;
-	double xIncrement, yIncrement;
+    double dx;
+    double dy;
+    double xIncrement, yIncrement;
 
     dx = x2 - x1;
     dy = y2 - y1;
@@ -50,150 +50,158 @@ void dda(t_data *data, double x1, double y1, double x2, double y2, int color)
     }
 }
 
-// void	dda(t_data *data,double xstart, double ystart,double xend, double yend,int color)
+// void dda(t_data *data,double xstart, double ystart,double xend, double yend,int color)
 // {
-// 	double	dx;
-// 	double	dy;
-// 	double	xinc;
-// 	double	yinc;
-// 	int		step;
+//  double  dx;
+//  double  dy;
+//  double  xinc;
+//  double  yinc;
+//  int     step;
 
-// 	dx = xstart - xend;
-// 	dy = ystart - yend;
-// 	step = ft_fabs(dx, dy);
-// 	xinc = dx / step;
-// 	yinc = dy / step;
-// 	while (step--)
-// 	{
-// 		if(xstart >=0 && xstart < WIDTH && ystart >=0 && ystart < HEIGHT)
-// 			mlx_put_pixel(data->image_win, xstart, ystart,color);
-// 		xstart = xstart + xinc;
-// 		ystart = ystart + yinc;
-// 	}
+//  dx = xstart - xend;
+//  dy = ystart - yend;
+//  step = ft_fabs(dx, dy);
+//  xinc = dx / step;
+//  yinc = dy / step;
+//  while (step--)
+//  {
+//      if(xstart >=0 && xstart < WIDTH && ystart >=0 && ystart < HEIGHT)
+//          mlx_put_pixel(data->image_win, xstart, ystart,color);
+//      xstart = xstart + xinc;
+//      ystart = ystart + yinc;
+//  }
 // }
 
 int angle_left_or_right(double angle) 
 {
     if (angle < (M_PI / 2) || angle > ((3 *M_PI)/2))
-		return 1;
+        return 1;
     return 0; // Angle is facing left
 }
 
 int angle_up_or_down(double angle) 
 {
-	if (angle > 0 && angle < M_PI)
-		return 1;
+    if (angle > 0 && angle < M_PI)
+        return 1;
     return 0; // Angle is facing up
 }
 
-int	wall_hit(double px, double py, t_data *data)
+int wall_hit(double px, double py, t_data *data)
 {
-	int	x;
-	int	y;
+    int x;
+    int y;
 
-	x = (px / Tile_size);
-	y = (py / Tile_size);
-	if (x > (data->width) || x < 0 || y < 0 || y > (data->height))
-		return (0);
-	if (x < data->width && x >= 0
-		&& y >= 0 && y < data->height && data->map[y][x] == '1')
-		return (1);
-	else
-		return (0);
+    x = (px / Tile_size);
+    y = (py / Tile_size);
+    if (x > data->width || x < 0 || y < 0 || y > data->height)
+        return (1);
+    if (x < data->width && x >= 0
+        && y >= 0 && y < data->height && data->map[y][x] == '1')
+        return (1);
+    else
+        return (0);
 }
 
 
 void castrayhorz(t_data *data, t_ray *ray,double ax, double ay,double dx,double dy, double angle)
 {
-	double y;
-	while (ax >= 0 && ay >= 0 && ax <= ((data->width) * Tile_size) && ay <= (data->height) * Tile_size)
-	{
-		if (!angle_up_or_down(angle))
-			y = ay - 1;
-		else
-			y = ay;
-		if (wall_hit(ax,y,data))
-		{
-			ray->hax = ax;
-			ray->hay = ay;
-			ray->horzhit = 1;
-			break ;
-		}
-		else 
-		{
-			ax += dx;
-			ay += dy;
-		}
-	}
+    double y;
+    while (ax >= 0 && ay >= 0 && ax <= ((data->width) * Tile_size) && ay <= (data->height) * Tile_size)
+    {
+        if (!angle_up_or_down(angle))
+            y = ay - 1;
+        else
+            y = ay;
+        if (wall_hit(ax,y,data))
+        {
+            ray->hax = ax;
+            ray->hay = ay;
+            ray->horzhit = 1;
+            break ;
+        }
+        else 
+        {
+            ax += dx;
+            ay += dy;
+        }
+    }
 }
 
 void horizontal(t_data *data, t_ray *ray, double angle)
 {
-	double ax;
-	double ay;
-	double dx;
-	double dy;
-	
-	dy = Tile_size;
-	dx = Tile_size / tan(angle);
-	if(!angle_up_or_down(angle))
-		dy *=-1;
-	ay = floor(data->player->py / Tile_size) * Tile_size;
-	if (angle_up_or_down(angle) > 0)
-		ay += Tile_size;
-	ax = data->player->px + ((ay - data->player->py) / tan(angle));
-	if ((!angle_left_or_right(angle) && dx > 0) || (angle_left_or_right(angle) && dx < 0))
-		dx *= -1.0;
-	castrayhorz(data,ray,ax,ay,dx,dy,angle);
+    double ax;
+    double ay;
+    double dx;
+    double dy;
+    
+    dy = 64.0;
+    dx = 64.0 / tan(angle);
+    if(!angle_up_or_down(angle))
+        dy *=-1;
+    ay = floor(data->player->py / 64.0) * 64.0;
+    if (angle_up_or_down(angle) > 0)
+        ay += 64.0;
+    ax = data->player->px + ((ay - data->player->py) / tan(angle));
+    if ((!angle_left_or_right(angle) && dx > 0) || (angle_left_or_right(angle) && dx < 0))
+        dx *= -1.0;
+    castrayhorz(data,ray,ax,ay,dx,dy,angle);
 }
 void castrayvert(t_data *data, t_ray *ray,double ax, double ay,double dx,double dy,double angle)
 {
-	double x;
-	while (ax >= 0 && ay >= 0 && ax <= ((data->width) * Tile_size) && ay <= (data->height) * Tile_size)
-	{
-		if (!angle_left_or_right(angle))
-			x = ax - 1;
-		else 
-			x = ax;
-		if (wall_hit(x,ay,data))
-		{
-			ray->vax = ax;
-			ray->vay = ay;
-			ray->verthit = 1;
-			break ;
-		}
-		else 
-		{
-			ax += dx;
-			ay += dy;
-		}
-	}
+    double x;
+    while (ax >= 0 && ay >= 0 && ax <= ((data->width) * Tile_size) && ay <= (data->height) * Tile_size)
+    {
+        if (!angle_left_or_right(angle))
+            x = ax - 1;
+        else 
+            x = ax;
+        if (wall_hit(x,ay,data))
+        {
+            ray->vax = ax;
+            ray->vay = ay;
+            ray->verthit = 1;
+            break ;
+        }
+        else 
+        {
+            ax += dx;
+            ay += dy;
+        }
+    }
 }
 void vertical(t_data *data, t_ray *ray, double angle)
 {
-	double ax;
-	double ay;
-	double dx;
-	double dy;
-	
-	ax = floor(data->player->px / Tile_size) * Tile_size;
-	dx = Tile_size;
-	if (angle_left_or_right(angle))
-		ax += Tile_size;
-	ay = data->player->py + ((ax - data->player->px) * tan(angle));
-	if (!angle_left_or_right(angle))
-		dx *= -1;
-	dy = Tile_size * tan(angle);
-	if ((!angle_up_or_down(angle) && dy > 0) || (angle_up_or_down(angle) && dy < 0))
-		dy *= -1;
-	castrayvert(data,ray,ax,ay,dx,dy,angle);
+    double ax;
+    double ay;
+    double dx;
+    double dy;
+    
+    ax = floor(data->player->px / 64.0) * 64.0;
+    dx = 64.0;
+    if (angle_left_or_right(angle))
+        ax += 64.0;
+    ay = data->player->py + ((ax - data->player->px) * tan(angle));
+    if (!angle_left_or_right(angle))
+        dx *= -1;
+    dy = Tile_size * tan(angle);
+    if ((!angle_up_or_down(angle) && dy > 0) || (angle_up_or_down(angle) && dy < 0))
+        dy *= -1;
+    castrayvert(data,ray,ax,ay,dx,dy,angle);
 }
 
-void norm_angle(double *angle)
+void norm_angle(double *angle, t_ray *ray)
 {
-	*angle = remainder(*angle, 2.0 * M_PI);
-	if (*angle < 0)
-		*angle = (2.0 * M_PI) + *angle;
+    *angle = remainder(*angle, 2.0 * M_PI);
+    if (*angle < 0)
+        *angle = (2.0 * M_PI) + *angle;
+    if (*angle > 0 && *angle < M_PI)
+        ray->ray_face_up = 0;
+    else
+        ray->ray_face_up= 1;
+    if (*angle < (0.5 * M_PI) || *angle > (1.5 * M_PI))
+        ray->ray_face_right = 1;
+    else
+        ray->ray_face_right = 0;
 }
 
 void ray_hit_deriction(t_data *data,t_ray *ray, double *xoffset, double angle)
@@ -213,135 +221,178 @@ void ray_hit_deriction1(t_data *data,t_ray *ray, double *xoffset, double angle)
 {
 	
 	if (angle_left_or_right(angle))
-	{	
-		ray->wall_deriction = EAST;
-		*xoffset = (fmod(ray->vay ,Tile_size) * data->east->width) / Tile_size;
-	}
-	else
 	{
 		ray->wall_deriction = WEST;
 		*xoffset = (fmod(ray->vay ,Tile_size) * data->west->width) / Tile_size;
 	}
-}
-
-void	set_color(t_colors *colors, mlx_texture_t *img, uint32_t	**pixels)
-{
-	colors->r = img->pixels[colors->count];
-	colors->g = img->pixels[colors->count + 1];
-	colors->b = img->pixels[colors->count + 2];
-	colors->a = img->pixels[colors->count + 3];
-	pixels[colors->i][colors->j] = ft_pixel(colors->r,
-			colors->g, colors->b, colors->a);
-}
-u_int32_t	**img_to_double_pointer(mlx_texture_t *img)
-{
-	uint32_t	**pixels;
-	t_colors	colors;
-
-	colors.i = 0;
-	colors.count = 0;
-	pixels = malloc(sizeof(uint32_t *) * img->height);
-	if (!pixels)
-		return (NULL);
-	while (colors.i < img->height)
+	else
 	{
-		colors.j = 0;
-		pixels[colors.i] = malloc(sizeof(uint32_t) * img->width);
-		if (!pixels[colors.i])
-			return (NULL);
-		while (colors.j < img->width)
-		{
-			set_color(&colors, img, pixels);
-			colors.j++;
-			colors.count += 4;
-		}
-		colors.i++;
+		ray->wall_deriction = EAST;
+		*xoffset = (fmod(ray->vay ,Tile_size) * data->east->width) / Tile_size;
 	}
-	return (pixels);
+}
+unsigned int    *set_texture_colors(mlx_texture_t *png_strct)
+{
+    unsigned int    i;
+    unsigned int    j;
+    unsigned int *texture;
+
+    i = 0;
+    j = 0;
+    texture = malloc(sizeof(unsigned int *) * (64 * 64));
+    while (png_strct->pixels[i] && (j < png_strct->width * png_strct->height))
+    {
+        texture[j] = ft_pixel(png_strct->pixels[i],
+                png_strct->pixels[i + 1],
+                png_strct->pixels[i + 2],
+                255);
+        j++;
+        i += png_strct->bytes_per_pixel;
+    }
+    return (texture);
+}
+
+uint32_t get_color(t_ray *ray,mlx_texture_t *texture,unsigned int yoffset,unsigned int xoffset)
+{
+    uint8_t *pixel;
+    int pxl_loction;
+    double height_divd;
+    int height;
+    
+    if (ray->project_plan > HEIGHT)
+        yoffset += (ray->project_plan - HEIGHT) / 2;
+    height_divd = texture->height / ray->project_plan;
+    height = floor((height_divd * yoffset)) * texture->width;
+    pxl_loction = height;
+    pxl_loction += xoffset;
+    pxl_loction *= texture->bytes_per_pixel;
+    pixel =&texture->pixels[pxl_loction];
+    return (pixel[0] << 24 | pixel[1] << 16 | pixel[2] << 8 | pixel[3]);
+}
+void    set_color(t_colors *colors, mlx_texture_t *img, uint32_t    **pixels)
+{
+    colors->r = img->pixels[colors->count];
+    colors->g = img->pixels[colors->count + 1];
+    colors->b = img->pixels[colors->count + 2];
+    colors->a = img->pixels[colors->count + 3];
+    pixels[colors->i][colors->j] = ft_pixel(colors->r,
+            colors->g, colors->b, colors->a);
+}
+u_int32_t   **img_to_double_pointer(mlx_texture_t *img)
+{
+    uint32_t    **pixels;
+    t_colors    colors;
+
+    colors.i = 0;
+    colors.count = 0;
+    pixels = malloc(sizeof(uint32_t *) * img->height);
+    if (!pixels)
+        return (NULL);
+    while (colors.i < img->height)
+    {
+        colors.j = 0;
+        pixels[colors.i] = malloc(sizeof(uint32_t) * img->width);
+        if (!pixels[colors.i])
+            return (NULL);
+        while (colors.j < img->width)
+        {
+            set_color(&colors, img, pixels);
+            colors.j++;
+            colors.count += 4;
+        }
+        colors.i++;
+    }
+    return (pixels);
 }
 
 void castallrays(t_data *data)
 {
-	int colum;
-	t_ray *ray;
-	double project_dist;
-	double ystart;
-	double yend;
-	double ray_start;
-	double player_ray_dist;
-	double ray_inc;
-	double xoffset;
-	double yoffset;
+    int colum;
+    t_ray *ray;
+    double project_dist;
+    double ystart;
+    double yend;
+    double ray_start;
+    double player_ray_dist;
+    double ray_inc;
+    double xoffset;
+    double yoffset;
+    unsigned int *texture;
+    uint32_t color;
 
-	ray_inc = FOV / WIDTH;
-	ray_start = data->player->rotationAngle - (FOV / 2.0);
-	colum = 0;
-	
-	while (colum < WIDTH)
-	{
-		
-		ray = malloc(sizeof(t_ray));
-		memset(ray, 0, sizeof(t_ray));//ft_
-		
-		ray->horzhit = 0;
-		ray->verthit = 0;
-		ray->distanceh = ULONG_MAX;
-		ray->distancev = ULONG_MAX;
-		norm_angle(&ray_start);
-		horizontal(data,ray,ray_start);
-		vertical(data,ray,ray_start);
-		ray->distanceh = sqrt(pow((ray->hax - data->player->px),2) + pow((ray->hay - data->player->py), 2));
-		ray->distancev = sqrt(pow((ray->vax -data->player->px),2) + pow((ray->vay - data->player->py), 2));
-		if((ray->distanceh < ray->distancev))
-			ray_hit_deriction(data,ray, &xoffset,ray_start);
-		else
-			ray_hit_deriction1(data,ray, &xoffset,ray_start);
-		if ((ray->distanceh < ray->distancev ) && ray->horzhit)
-			player_ray_dist = ray->distanceh * cos(data->player->rotationAngle - ray_start);
-		else
-			player_ray_dist = ray->distancev * cos(data->player->rotationAngle - ray_start);
-		project_dist = (WIDTH / 2)/tan(FOV / 2);
-		ray->project_plan = (project_dist / player_ray_dist) * Tile_size;
-		ystart = (HEIGHT / 2) - (ray->project_plan / 2);
-		yend = (HEIGHT / 2) + (ray->project_plan / 2);
-		if (ystart < 0)
-			ystart = 0;
-		while (ystart < yend && ystart < HEIGHT)
-		{
-			// if (ray->wall_deriction == NORTH)
-			// {
-			// 	yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
-			// 			* data->north->height) / ray->project_plan;
-			// 	mlx_put_pixel(data->image_win ,colum ,ystart, data->no[(int)yoffset][(int)xoffset]);
-			// 	// mlx_put_pixel(data->image_win ,colum ,ystart, ft_pixel(255,0,0,255));
-			// }
-			// if (ray->wall_deriction == SOUTH)
-			// {
-			// 	yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
-			// 			* data->south->height) / ray->project_plan;
-			// 	mlx_put_pixel(data->image_win ,colum ,ystart, data->so[(int)yoffset][(int)(data->south->width - 1) - ((int)xoffset)]);
-			// 	// mlx_put_pixel(data->image_win ,colum ,ystart, ft_pixel(255,255,0,255));
-			// }
-			// if (ray->wall_deriction == WEST)
-			// {
+    color = 0;
+    ray_inc = FOV / WIDTH;
+    ray_start = data->player->rotationAngle - (FOV / 2.0);
+    colum = 0;
+    texture = malloc(sizeof(unsigned int *)*(64 * 64));
+    data->no = img_to_double_pointer(data->north);
+    while (colum < WIDTH)
+    {
+        
+        ray = malloc(sizeof(t_ray));
+        memset(ray, 0, sizeof(t_ray));
+        ray->horzhit = 0;
+        ray->verthit = 0;
+        ray->distanceh = INT_MAX;
+        ray->distancev = INT_MAX;
+        norm_angle(&ray_start, ray);
+        horizontal(data,ray,ray_start);
+        vertical(data,ray,ray_start);
+        if(ray->horzhit)
+            ray->distanceh = sqrt(pow((ray->hax - data->player->px), 2) + pow((ray->hay - data->player->py), 2));
+        if(ray->verthit)
+            ray->distancev = sqrt(pow((ray->vax -data->player->px), 2) + pow((ray->vay - data->player->py), 2));
+        if ((ray->distanceh < ray->distancev ) && ray->horzhit)
+        {
+            player_ray_dist = ray->distanceh * cos(data->player->rotationAngle - ray_start);
+            ray_hit_deriction(data,ray, &xoffset,ray_start);
+            ray->horzhit = 1;
+            ray->verthit = 0;
+        }
+        else
+        {
+            player_ray_dist = ray->distancev * cos(data->player->rotationAngle - ray_start);
+            ray_hit_deriction1(data,ray, &xoffset,ray_start);
+            ray->horzhit = 0;
+            ray->verthit = 1;
+        }
+        project_dist = (WIDTH / 2)/tan(FOV / 2);
+        ray->project_plan = (project_dist / player_ray_dist) * Tile_size;
+        ystart = (HEIGHT / 2) - (ray->project_plan / 2);
+        yend = (HEIGHT / 2) + (ray->project_plan / 2);
+        if (ystart < 0)
+            ystart = 0;
+        while (ystart < yend && ystart < HEIGHT)
+        {
+			if (ray->wall_deriction == NORTH)
+			{
+				yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
+						* data->north->height) / ray->project_plan;
+				mlx_put_pixel(data->image_win ,colum ,ystart, data->no[(int)yoffset][(int)xoffset]);
+			}
+			if (ray->wall_deriction == SOUTH)
+			{
+				yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
+						* data->south->height) / ray->project_plan;
+				mlx_put_pixel(data->image_win ,colum ,ystart, data->so[(int)yoffset][(int)(data->south->width - 1) - ((int)xoffset)]);
+			}
+			if (ray->wall_deriction == WEST)
+			{
 				yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
 						* data->west->height) / ray->project_plan;
 				mlx_put_pixel(data->image_win ,colum ,ystart, data->we[(int)yoffset][(int)(data->west->width - 1) - ((int)xoffset)]);
-				// mlx_put_pixel(data->image_win ,colum ,ystart, ft_pixel(0,0,255,255));
-			// }
-			// if (ray->wall_deriction == EAST)
-			// {
-				// yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
-						// * data->east->height) / ray->project_plan;
-				// mlx_put_pixel(data->image_win ,colum ,ystart, data->ea[(int)yoffset][(int)xoffset]);
-				// mlx_put_pixel(data->image_win ,colum ,ystart, ft_pixel(0,255,255,255));
-			// }
-			
-			ystart++;
-			
-		}
-		ray_start += ray_inc;
-		colum++;
-		free(ray);
-	}
+			}
+			if (ray->wall_deriction == EAST)
+			{
+				yoffset = ((ystart - ((HEIGHT / 2.0) - (ray->project_plan / 2.0))) \
+						* data->east->height) / ray->project_plan;
+				mlx_put_pixel(data->image_win ,colum ,ystart, data->ea[(int)yoffset][(int)xoffset]);
+			}
+            ystart++;
+        }
+        free(ray);
+        ray_start += ray_inc;
+        colum++;
+    }
 }
+
--- src/render.c
+++ src/render.c
@@ -6,81 +6,12 @@
 /*   By: asabri <asabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 20:55:33 by asabri            #+#    #+#             */
-/*   Updated: 2023/11/18 16:13:26 by asabri           ###   ########.fr       */
+/*   Updated: 2023/11/19 10:48:29 by asabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-// void dda(t_data *data, double x1, double y1, double x2, double y2, int color)
-// {
-//     int i;
-//     double steps;
-// 	double dx;
-// 	double dy;
-// 	double xIncrement, yIncrement;
-
-//     dx = x2 - x1;
-//     dy = y2 - y1;
-//     i = 0;
-//     steps = fmax(fabs(dx), fabs(dy));
-//     if (steps == 0)
-//     {
-//         mlx_put_pixel(data->image_win, round(x1), round(y1), color);
-//         return;
-//     }
-//     xIncrement = dx / steps;
-//     yIncrement = dy / steps;
-//     while (i <= steps)
-//     {
-//         if ((x1) > WIDTH || (x1) < 0 || (y1) < 0 || (y1) > HEIGHT)
-//             return;
-//         mlx_put_pixel(data->image_win, round(x1), round(y1), color);
-//         x1 += xIncrement;
-//         y1 += yIncrement;
-//         i++;
-//     }
-// }
-void init(t_data *data);
-//(255,255,0)yellow color
-// void draw_player(t_data *data, uint32_t color)
-// {
-//     (void)color;
-//     for (int i = 0; i < 360; i++)
-//         dda(data,data->player->px ,data->player->py ,data->player->px + (cos(i * PI/180) * 10),data->player->py + (sin(i * PI/180) * 10),ft_pixel(255,0,0,255));
-//     dda(data,data->player->px,data->player->py,data->player->px + (cos(data->player->rotationAngle) * 30),data->player->py + (sin(data->player->rotationAngle) * 30),ft_pixel(255,0,0,255));
-// }
-// void draw_square(t_data *data, int y,int x, uint32_t color)
-// {
-//     (void)color;
-//     for (int i = 0; i < Tile_size - 1; i++)
-//         for (int j = 0; j < Tile_size - 1; j++)
-//         {
-//             mlx_put_pixel(data->image_win, (x + i) * MINI_MAP_SCALE, (y + j) * MINI_MAP_SCALE, ft_pixel(color,color,color,color));
-//         }
-// }
-// void draw_map(t_data *data)
-// {
-//     int i;
-//     int j;
-
-//     i = -1;
-//     data->width = 0;
-//     data->height = 0;
-//     while (data->map[++i])
-//     {
-//         j = -1;
-//         while (data->map[i][++j])
-//         {
-//             if (data->map[i][j] == '1')
-//                 draw_square(data,i * Tile_size, j * Tile_size , 0x5a5a5a);
-//             else 
-//                 draw_square(data,i * 64, j * 64, 0x808080);
-//             data->width++;
-//         }
-//         data->height++;
-//     }
-// }
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
     return (r << 24 | g << 16 | b << 8 | a);
@@ -239,10 +170,11 @@ void    get_player_pos(t_data *data)
                 data->player->px = ((x * Tile_size) + (Tile_size /2));
                 data->player->py = ((y * Tile_size) + (Tile_size / 2));
                 data->player->rotationAngle = check_angle(data->map[y][x]);
-                data->map[y][x] = '0';
             }
         }
     }
+    // printf("%f\n",data->player->px);
+    // printf("%f\n",data->player->py);
 }
 
 void ft_keyfunc_relesed(mlx_key_data_t keypress, t_data *data)
@@ -325,11 +257,11 @@ void ft_hook(void *param)
         ft_renderplayer1(data);
     mlx_delete_image(data->mlx,data->image_win);
     if (!(data->image_win = mlx_new_image(data->mlx, WIDTH, HEIGHT)))
-	{
-		mlx_close_window(data->mlx);
-		puts(mlx_strerror(mlx_errno));
-		return((void)EXIT_FAILURE);
-	}
+    {
+        mlx_close_window(data->mlx);
+        puts(mlx_strerror(mlx_errno));
+        return((void)EXIT_FAILURE);
+    }
     mlx_image_to_window(data->mlx, data->image_win, 0, 0);
     fill_window(data);
     mouse_move(data);
@@ -357,21 +289,22 @@ void init(t_data *data)
     mlx_set_setting(MLX_STRETCH_IMAGE, true);
     data->mlx = mlx_init(WIDTH,HEIGHT,"cub3d",true);
     if (!(data->mlx))
-		return (puts(mlx_strerror(errno)),(void)EXIT_FAILURE);
+        return (puts(mlx_strerror(errno)),(void)EXIT_FAILURE);
     if (!(data->image_win = mlx_new_image(data->mlx, WIDTH, HEIGHT)))
-	{
-		mlx_close_window(data->mlx);
-		puts(mlx_strerror(mlx_errno));
-		return((void)EXIT_FAILURE);
-	}
+    {
+        mlx_close_window(data->mlx);
+        puts(mlx_strerror(mlx_errno));
+        return((void)EXIT_FAILURE);
+    }
     if (init_textures(&data))
         exit (1);
     mlx_set_cursor_mode(data->mlx,MLX_MOUSE_HIDDEN);
-    get_player_pos(data);
+
     data->no = img_to_double_pointer(data->north);
 	data->so = img_to_double_pointer(data->south);
     data->we = img_to_double_pointer(data->west);
 	data->ea = img_to_double_pointer(data->east);
+    get_player_pos(data);
     mlx_set_cursor_mode(data->mlx,MLX_MOUSE_HIDDEN);
     mlx_image_to_window(data->mlx, data->image_win, 0, 0);
     mlx_key_hook(data->mlx,ft_keyfunc_pressed,data);
