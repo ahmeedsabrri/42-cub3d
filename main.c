e763058dbee04ca64975d5fc1b0f8d280d028fb1
Abdel fattah Berkach <116562268+aberkach@users.noreply.github.com>
2023-11-23 15:47:26 +0100

    NOTE : i fixed some protection in code + zidt whda fl main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abberkac <abberkac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 01:33:47 by abberkac          #+#    #+#             */
-/*   Updated: 2023/11/23 15:41:40 by abberkac         ###   ########.fr       */
+/*   Updated: 2023/11/23 15:47:06 by abberkac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,8 +123,8 @@ int	main(int ac, char **av)
 		ft_error("Error: Should be one argument\n");
 	data = (t_data *)ft_malloc(sizeof(t_data), 1);
 	player = (t_player *)ft_malloc(sizeof(t_player), 1);
-	if (!data || parsing(av, data))
-		return (ft_malloc(0, 0), 2);
+	if (!player || !data || parsing(av, data))
+		return (ft_malloc(0, 0), 1);
 	data->width = width_size(data->map);
 	data->height = get_height(data->map);
 	player->rotat_angle = PI / 2;
